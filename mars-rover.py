285d639998411a4c4f19e1d66a31910e1fdad26a
Octavio <rusmanoctavio@gmail.com>
2026-08-24 19:24:06 -0300

    Requerimiento 1 Verde

--- mars-rover.py
+++ mars-rover.py
@@ -1,6 +1,12 @@
 orientacion = 'N'
 posicion = (0,0)
 
+def get_posicion():
+    return posicion
+
+def get_orientacion():
+    return orientacion
+
 def test_posicion():
     assert (0,0) == get_posicion()
 
